07d9128791331feffdbb0e6dcded23a6d22f3726
Chris Maloney <voldrani@gmail.com>
2013-10-27 10:37:14 -0400

    Work on the tutorial, learning about active record

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131025034433) do
+ActiveRecord::Schema.define(:version => 20131026032525) do
+
+  create_table "people", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.integer  "age"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "students", :force => true do |t|
     t.string   "username"
